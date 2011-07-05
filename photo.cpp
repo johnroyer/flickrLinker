c13c3beece367a27c45c480adc43effcdd0aff4b
Zero <john.royer@gmail.com>
2011-07-05 22:38:28 +0800

    Adding operator << and >> for photo
    Adding operator << and >> for QDatastream in Photo. Photo in operator <<
    should be fix to const.

--- photo.cpp
+++ photo.cpp
@@ -1,5 +1,36 @@
 #include "photo.h"
+#include <QDataStream>
 
 Photo::Photo()
 {
+}
+
+void Photo::setTitle(QString title){
+    this->title = title;
+}
+
+void Photo::setUniqUrl(QString uniqUrl){
+    this->uniqUrl = uniqUrl;
+}
+
+QString Photo::getUniqUrl(){
+    return this->getUniqUrl();
+}
+
+QString Photo::getTitle(){
+    return this->title;
+}
+
+QDataStream &operator<<(QDataStream &out, Photo &p){
+    out << p.getTitle();
+    return out;
+}
+
+QDataStream &operator>>(QDataStream &in, Photo &photo){
+    QString url;
+    QString title;
+    in >> url >> title;
+    photo.setUniqUrl(title);
+
+    return in;
 }
--- photo.h
+++ photo.h
@@ -9,17 +9,9 @@ public:
     Photo();
     Photo(QString);
     void setUniqUrl(QString);
+    void setTitle(QString);
     QString getUniqUrl();
     QString getTitle();
-    QString getAlt();
-    QString getThumbUrl();
-    QString getMediumUrl();
-    QString getLargeUrl();
-    QString getOrigUrl();
-    QString getThumbImage();
-    QString getMediumImage();
-    QString getLargeImage();
-    QString getOrigImage();
 
 private:
     QString uniqUrl;
